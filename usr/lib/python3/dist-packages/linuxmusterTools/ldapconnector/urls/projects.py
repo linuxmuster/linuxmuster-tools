826ed3a87e2255eb25362abcd1c1dcf63f5001f2
kiarn <arnaud@kientz.me>
2023-08-05 23:38:07 +0200

    Projects requests.

---
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/urls/projects.py
@@ -0,0 +1,21 @@
+import linuxmusterTools.ldapconnector.models as models
+from linuxmusterTools.ldapconnector.urls.ldaprouter import router
+
+
+@router.single(r'/projects/(?P<project>[a-z0-9_\-]*)', models.LMNProject)
+def get_project(project):
+    """
+    Get all details from a specific project.
+    Return a LMNProject data object
+    """
+
+    return f"""(&(cn={project})(objectClass=group)(sophomorixType=project))"""
+
+@router.collection(r'/projects', models.LMNProject)
+def get_all_projects():
+    """
+    Get all projects details.
+    Return a list of LMNProject data objects.
+    """
+
+    return """(&(objectClass=group)(sophomorixType=project))"""
