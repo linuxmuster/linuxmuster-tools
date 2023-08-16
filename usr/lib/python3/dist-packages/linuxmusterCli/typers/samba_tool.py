5a3bd904773f6e6d0491e36b545d7060b9836d13
kiarn <arnaud@kientz.me>
2023-08-16 17:07:07 +0200

    Add drives.

--- usr/lib/python3/dist-packages/linuxmusterCli/typers/samba_tool.py
+++ usr/lib/python3/dist-packages/linuxmusterCli/typers/samba_tool.py
@@ -1,4 +1,5 @@
 import typer
+from typing_extensions import Annotated
 
 from rich.console import Console
 from rich.table import Table
@@ -20,3 +21,22 @@ def gpos():
     for name, details in GPOS.items():
         gpos.add_row(name, details.gpo, details.path)
     console.print(gpos)
+
+@app.command()
+def drives(school: Annotated[str, typer.Option("--school", "-s")] = 'default-school'):
+    drives = Table()
+    drives.add_column("Name", style="green")
+    drives.add_column("Letter", style="cyan")
+    drives.add_column("Use letter", style="cyan")
+    drives.add_column("Label", style="magenta")
+    drives.add_column("Disable", style="magenta")
+
+    for name, details in GPOS[f"sophomorix:school:{school}"].drives.drives_dict.items():
+        drives.add_row(
+                name, 
+                details['letter'], 
+                str(details['userLetter']), 
+                details['label'], 
+                str(details['disabled'])
+        )
+    console.print(drives)
