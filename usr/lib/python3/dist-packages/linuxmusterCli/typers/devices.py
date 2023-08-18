c482b05fabdd051739cc6e08e8b537a2ca68523d
kiarn <arnaud@kientz.me>
2023-08-18 21:55:18 +0200

    Filter devices output.

--- usr/lib/python3/dist-packages/linuxmusterCli/typers/devices.py
+++ usr/lib/python3/dist-packages/linuxmusterCli/typers/devices.py
@@ -12,14 +12,25 @@ console = Console(emoji=False)
 app = typer.Typer()
 
 @app.command()
-def ls(school: Annotated[str, typer.Option("--school", "-s")] = 'default-school'):
+def ls(
+        school: Annotated[str, typer.Option("--school", "-s")] = 'default-school',
+        filter_str: Annotated[str, typer.Option("--filter", "-f")] = ''
+        ):
     if school != 'default-school':
         prefix = f'{school}.'
     else:
         prefix = ''
-    
+   
+    filter_str = filter_str.lower()
+
     with LMNFile(f'/etc/linuxmuster/sophomorix/{school}/{prefix}devices.csv', 'r') as f:
-        devices_data = sorted(f.read(), key=lambda d: (d['room'], d['hostname']))
+        devices_data = list(filter(lambda d:d['room'][0] != "#", f.read()))
+        devices_data = sorted(devices_data, key=lambda d: (d['room'], d['hostname']))
+
+    devices_data = list(filter(
+        lambda d: filter_str in d['hostname'].lower() or filter_str in d['mac'].lower() or filter_str in d['room'].lower(), 
+        devices_data
+    ))
 
     ldap_data = lr.get('/devices', attributes=['cn', 'sophomorixComputerMAC'])
 
@@ -30,12 +41,11 @@ def ls(school: Annotated[str, typer.Option("--school", "-s")] = 'default-school'
     devices.add_column("Mac", style="magenta")
     devices.add_column("LDAP", style="magenta")
     for device in devices_data:
-        if device['room'][0] != "#":
-            for ldap_device in ldap_data:
-                if device['hostname'].lower() == ldap_device['cn'].lower() and device['mac'].lower() == ldap_device['sophomorixComputerMAC'].lower():
-                    devices.add_row(device['room'], device['hostname'], device['ip'], device['mac'], "Registered")
-                    break
-            else:
-                devices.add_row(device['room'], device['hostname'], device['ip'], device['mac'], "Not registered")
+        for ldap_device in ldap_data:
+            if device['hostname'].lower() == ldap_device['cn'].lower() and device['mac'].lower() == ldap_device['sophomorixComputerMAC'].lower():
+                devices.add_row(device['room'], device['hostname'], device['ip'], device['mac'], "Registered")
+                break
+        else:
+            devices.add_row(device['room'], device['hostname'], device['ip'], device['mac'], "Not registered")
     console.print(devices)
 
