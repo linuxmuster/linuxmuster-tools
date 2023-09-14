a85bfd4032be6bc59a6b65ae080663d251a943cd
kiarn <arnaud@kientz.me>
2023-09-14 21:11:12 +0200

    Add LMNGroup.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/__init__.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/__init__.py
@@ -4,3 +4,4 @@ from .lmnschoolclass import *
 from .lmnproject import *
 from .lmnschool import *
 from .lmndevice import *
+from .lmngroup import *
--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/lmngroup.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/lmngroup.py
@@ -2,23 +2,18 @@ from dataclasses import dataclass, field
 
 
 @dataclass
-class LMNDevice:
+class LMNGroup:
     cn: str
     displayName: str
     distinguishedName: str
     memberOf: list
     name: str
-    proxyAddresses: list
     sAMAccountName: str
     sAMAccountType: str
     sophomorixAdminClass: str
-    sophomorixAdminFile: str
-    sophomorixComment: str
-    sophomorixComputerIP: str
-    sophomorixComputerMAC: str
-    sophomorixComputerRoom: str
     sophomorixCreationDate: str
     sophomorixRole: str
     sophomorixSchoolname: str
     sophomorixSchoolPrefix: str
     sophomorixStatus: str
+    sophomorixType: str
