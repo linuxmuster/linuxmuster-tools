9187f6b1bcacc5a9b749a9fa2fb8537b0029263a
kiarn <arnaud@kientz.me>
2024-01-18 13:11:17 +0100

    Get first pw list for sessions.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/lmnsession.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/lmnsession.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
+from ..urls import router as lr
+
 
 @dataclass
 class LMNSession:
@@ -7,3 +9,14 @@ class LMNSession:
     name: str
     members: list
     membersCount: int
+
+    def get_first_passwords(self):
+        response = {}
+
+        for member in self.members:
+            lmnuser = lr.get(f'/users/{member}', dict=False)
+            response[member] = {
+                'firstPassword': lmnuser.sophomorixFirstPassword,
+                'firstPasswordStillSet': lmnuser.test_first_password(),
+            }
+        return response
