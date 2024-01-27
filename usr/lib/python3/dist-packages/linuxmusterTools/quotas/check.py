c97c28a96c5ecc5af80ff8678c84da2fc053be3e
kiarn <arnaud@kientz.me>
2024-01-27 14:15:46 +0100

    Get user quotas.

--- usr/lib/python3/dist-packages/linuxmusterTools/quotas/check.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/quotas/check.py
@@ -1,8 +1,10 @@
 import os
 import pwd
+import re
+import subprocess
 
-from ..samba_util import SAMBA_WORKGROUP
-
+from ..samba_util import SAMBA_WORKGROUP, SAMBA_DOMAIN
+from ..ldapconnector import LMNLdapReader as lr
 
 def list_user_files(user):
     path = '/srv/samba'
@@ -32,3 +34,44 @@ def list_user_files(user):
         directories[directory]['total'] = f"{mega:.2f}"
 
     return {'directories': directories, 'total': f"{total / 1024 / 1024:.2f}"}
+
+def get_user_quotas(user):
+    # TODO: find a better way to get shares list
+    sophomorixQuota = lr.getval(f'/users/{user}', 'sophomorixQuota')['sophomorixQuota']
+    if sophomorixQuota is None:
+        raise Exception(f'User {user} not found in ldap')
+
+    quotas = {share.split(":")[0]: None for share in sophomorixQuota}
+
+    with open('/etc/linuxmuster/.secret/administrator', 'r') as adm_pw:
+        pw = adm_pw.readline().strip()
+
+    def _format_quota(quota):
+        if "NO LIMIT" in quota:
+            return quota
+        return round(int(quota) / 1024 /1024, 2)
+
+    # TODO: parallel ?
+    for share in quotas.keys():
+        cmd = ['smbcquotas', '-U', f'administrator%{pw}', '-u', user, f'//{SAMBA_DOMAIN}/{share}']
+        smbc_output = subprocess.run(cmd, capture_output=True)
+        out, err = smbc_output.stdout.decode().strip(),  smbc_output.stderr.decode().strip()
+        if 'cli_full_connection failed!' in err:
+            # Catch Samba error
+            error_code =  err.split("(")[1].strip(")")
+            quotas[share] = {
+                "used": error_code,
+                "soft_limit": error_code,
+                "hard_limit": error_code,
+            }
+        else:
+            used, soft, hard = [value.strip("/") for value in re.split(r"\s{2,}", out)[2:]]
+            quotas[share] = {
+                "used": _format_quota(used),
+                "soft_limit": _format_quota(soft),
+                "hard_limit": _format_quota(hard),
+            }
+
+    pw = ''
+
+    return quotas
