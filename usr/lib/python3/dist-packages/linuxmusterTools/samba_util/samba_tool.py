e822e113434643783bc0e9e271a97892ff2e07e4
kiarn <arnaud@kientz.me>
2024-02-08 09:28:04 +0100

    Add group manager.

--- usr/lib/python3/dist-packages/linuxmusterTools/samba_util/samba_tool.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/samba_util/samba_tool.py
@@ -1,8 +1,9 @@
 import os
 import logging
+import subprocess
 from dataclasses import dataclass, field, asdict
 from .drives import DriveManager
-
+from ..ldapconnector import LMNLdapReader as lr
 
 try:
     from samba.auth import system_session
@@ -55,3 +56,85 @@ class GPOManager:
             unix_path = "/var/lib/samba/" + '/'.join(path.split('\\')[3:])
             drivemgr = DriveManager(unix_path)
             self.gpos[name] = GPO(str(gpo.dn), drivemgr, gpo_id, name, path, unix_path)
+
+class GroupManager:
+    """
+    Samble class to manage samba groups via samba-tool.
+    """
+
+    def __init__(self):
+        self.POST_HOOK_DIR = '/etc/linuxmuster/tools/hooks/group-manager'
+
+        if os.path.isfile(SAMDB_PATH):
+            try:
+                self.samdb = SamDB(url=SAMDB_PATH, session_info=system_session(),credentials=creds, lp=lp)
+            except Exception:
+                logging.error(f'Could not load {SAMDB_PATH}, is linuxmuster installed ?')
+        else:
+            logging.warning(f'{SAMDB_PATH} not found, is linuxmuster installed ?')
+
+    def _run_post_hook(self, action, group, members):
+        """
+        Runs alls the scripts located in self.POST_HOOK_DIR in alphanum order, passing 3 arguments:
+
+        :param action: actually 'remove' or 'add'
+        :type action: basestring
+        :param group: group name
+        :type group: basestring
+        :param members: members list, will be given coma separated as argument
+        :type members: list
+        """
+
+        for script in sorted(os.listdir(self.POST_HOOK_DIR)):
+            print(f"Executing {os.path.join(self.POST_HOOK_DIR, script)}")
+            subprocess.run([os.path.join(self.POST_HOOK_DIR, script), action, group, ','.join(members)])
+
+    def list(self):
+        raw_groups = lr.get('/groups', attributes=['cn', 'sophomorixType'])
+        groups = {}
+
+        for group in raw_groups:
+            if group['sophomorixType'] not in groups:
+                groups[group['sophomorixType']] = []
+            groups[group['sophomorixType']].append(group['cn'])
+
+        return groups
+
+    def remove_members(self, group, members):
+        """
+        Remove members from a group. samdb throw an Exception if the group or the user
+        does not exist.
+
+        :param group: group name
+        :type group: basestring
+        :param members: members list
+        :type members: list
+        """
+
+        try:
+            self.samdb.add_remove_group_members(groupname=group, members=members, add_members_operation=False)
+        except Exception as e:
+            print(e)
+            return
+
+        self._run_post_hook('remove', group, members)
+
+    def add_members(self, group, members):
+        """
+        Add members to a group. samdb throw an Exception if the group or the user
+        does not exist.
+
+        :param group: group name
+        :type group: basestring
+        :param members: members list
+        :type members: list
+        """
+
+        try:
+            self.samdb.add_remove_group_members(groupname=group, members=members, add_members_operation=True)
+        except Exception as e:
+            print(e)
+            return
+
+        self._run_post_hook('add', group, members)
+
