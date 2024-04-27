3709db3a7227ce202db7b6aed1ff9aeb241ed092
kiarn <arnaud@kientz.me>
2024-04-27 21:43:09 +0200

    Get a specific management group.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/urls/managementgroups.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/urls/managementgroups.py
@@ -2,12 +2,26 @@ import linuxmusterTools.ldapconnector.models as models
 from linuxmusterTools.ldapconnector.urls.ldaprouter import router, SCHOOL_MARKER
 
 @router.collection_s(r'/managementgroups', models.LMNGroup, subdn=f'OU=Management,OU={SCHOOL_MARKER},OU=SCHOOLS,')
-def get_all_groups():
+def get_all_management_groups():
     """
-    Get all groups.
+    Get all management groups.
     Return a list of LMNGroup data objects.
     """
 
     ldap_filter = "(objectClass=group)"
+
+    return ldap_filter
+
+@router.single_s(r'/managementgroups/(?P<name>[\w\-_]*)', models.LMNGroup, subdn=f'OU=Management,OU={SCHOOL_MARKER},OU=SCHOOLS,')
+def get_management_group(name=''):
+    """
+    Get a management group specified by its name.
+    Return a LMNGroup data object.
+    """
+
+    ldap_filter = f"""(&
+                            (cn={name})
+                            (objectClass=group)
+            )"""
 
     return ldap_filter
