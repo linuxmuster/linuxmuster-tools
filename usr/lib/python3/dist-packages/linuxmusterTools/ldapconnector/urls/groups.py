5264846cbd8b10c900de7ece8237a2fb7435c257
kiarn <arnaud@kientz.me>
2024-06-28 08:36:13 +0200

    Difference between units and sophomrix-group.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/urls/groups.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/urls/groups.py
@@ -1,10 +1,12 @@
 import linuxmusterTools.ldapconnector.models as models
 from linuxmusterTools.ldapconnector.urls.ldaprouter import router, SCHOOL_MARKER
 
-@router.collection_s(r'/groups', models.LMNGroup, subdn=f'OU={SCHOOL_MARKER},OU=SCHOOLS,')
-def get_all_groups():
+
+@router.collection_s(r'/units', models.LMNGroup, subdn=f'OU={SCHOOL_MARKER},OU=SCHOOLS,')
+def get_all_units():
     """
-    Get all groups.
+    Get all units. The terminology "unit" was chosen in order to differenciate with a "group" from
+    sophomorix-group. A unit can be a schoolclass, a project, a group, etc ...
     Return a list of LMNGroup data objects.
     """
 
@@ -12,15 +14,46 @@ def get_all_groups():
 
     return ldap_filter
 
-@router.single_s(r'/groups/(?P<name>[\w\-_]*)', models.LMNGroup, subdn=f'OU={SCHOOL_MARKER},OU=SCHOOLS,')
+@router.single_s(r'/units/(?P<name>[\w\-_]*)', models.LMNGroup, subdn=f'OU={SCHOOL_MARKER},OU=SCHOOLS,')
 def get_group(name=''):
     """
-    Get a group specified by its name.
+    Get a unit specified by its name. The terminology "unit" was chosen in order to differenciate with a "group" from
+    sophomorix-group. A unit can be a schoolclass, a project, a group, etc ...
     Return a LMNGroup data object.
     """
 
     ldap_filter = f"""(&
                             (cn={name})
+                            (objectClass=group)
+            )"""
+
+    return ldap_filter
+
+
+@router.collection_s(r'/groups', models.LMNGroup, subdn=f'OU={SCHOOL_MARKER},OU=SCHOOLS,')
+def get_all_groups():
+    """
+    Get all groups (meaning units with sophomorixType sophomorix-group).
+    Return a list of LMNGroup data objects.
+    """
+
+    ldap_filter = f"""(&
+                            (sophomorixType=sophomorix-group)
+                            (objectClass=group)
+            )"""
+
+    return ldap_filter
+
+@router.single_s(r'/groups/(?P<name>[\w\-_]*)', models.LMNGroup, subdn=f'OU={SCHOOL_MARKER},OU=SCHOOLS,')
+def get_group(name=''):
+    """
+    Get a group (meaning units with sophomorixType sophomorix-group) specified by its name.
+    Return a LMNGroup data object.
+    """
+
+    ldap_filter = f"""(&
+                            (cn={name})
+                            (sophomorixType=sophomorix-group)
                             (objectClass=group)
             )"""
 
