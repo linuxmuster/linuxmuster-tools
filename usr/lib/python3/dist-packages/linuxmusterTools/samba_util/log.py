40d287ac3679ceb5f6f3b1965d4861359e50088e
kiarn <arnaud@kientz.me>
2024-07-18 15:29:59 +0200

    Check audti level.

--- usr/lib/python3/dist-packages/linuxmusterTools/samba_util/log.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/samba_util/log.py
@@ -7,6 +7,19 @@ from ..samba_util import SAMBA_REALM, LOG_LEVEL
 SAMBA_LOG = '/var/log/samba/log.samba'
 SAMBA_LOG_OLD = '/var/log/samba/log.samba.1'
 
+def check_audit_level():
+    auth_audit = LOG_LEVEL.get('auth_audit', 1)
+    general = LOG_LEVEL.get('general', 1)
+
+    if auth_audit > 2:
+        return True
+
+    if general > 2:
+        return True
+
+    logging.error("Can not parse log files from samba, you need to set the log level of auth_audit at least at 3.")
+    return False
+
 def format_log_data(entry):
     data = re.findall(r"\[([^\]]*)\]", entry)
     if "@" in data[2]:
@@ -28,10 +41,13 @@ def parse_log_files(log_file, pattern):
     return logs
 
 def last_login(pattern):
-    logs = []
-    logs.extend(parse_log_files(SAMBA_LOG, pattern))
-    logs.extend(parse_log_files(SAMBA_LOG_OLD, pattern))
-    for entry in logs:
-        print(f"{entry['user']:25}|{entry['ip']:20}|{entry['datetime']}")
-    return logs
 
+    if check_audit_level():
+        logs = []
+        logs.extend(parse_log_files(SAMBA_LOG, pattern))
+        logs.extend(parse_log_files(SAMBA_LOG_OLD, pattern))
+        for entry in logs:
+            print(f"{entry['user']:25}|{entry['ip']:20}|{entry['datetime']}")
+        return logs
+
+    return
