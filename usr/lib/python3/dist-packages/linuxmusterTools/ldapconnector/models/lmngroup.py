692d9a5c8e425c3e269f78f22272b9cf69eb8359
kiarn <arnaud@linuxmuster.net>
2024-09-05 19:50:15 +0200

    Get all members for a group.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/lmngroup.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/lmngroup.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass, asdict
+from ..urls import router as lr
+from .lmnobject import LMNObject
 
 
 @dataclass
@@ -33,3 +35,52 @@ class LMNGroup:
 
     def asdict(self):
         return asdict(self)
+
+    def get_all_members(self):
+        """
+        Set a members and admins list by recursively scanning all groups and their members which are members
+        or admins of this project.
+
+        This is not called as post init because of nested projects. So it's necessary to call this method
+        afterwards:
+
+        p = lr.get('/projects/p_test', dict=False)
+        p.get_all_members()
+        """
+
+        members = set()
+        to_scan = []
+
+        for dn in self.member:
+            details = lr.get_from_dn(LMNObject, dn)
+            if 'person' in details["objectClass"]:
+                members.add(details['cn'])
+            elif 'group' in details["objectClass"]:
+                to_scan.append(details['cn'])
+
+        already_scanned = []
+
+        while to_scan:
+            group = to_scan[0]
+            if group not in already_scanned:
+                already_scanned.append(group)
+
+                # I assume a group is a project or a schoolclass
+                if group.startswith('p_'):
+                    group_details = lr.get(f'/projects/{group}', attributes=['sophomorixMembers', 'sophomorixMemberGroups'])
+                    group_members = set(group_details.get('sophomorixMembers', []))
+                    subgroups = group_details.get('sophomorixMemberGroups', [])
+
+                    # Subgroups must eventually come in the scan list
+                    for subgroup in subgroups:
+                        if subgroup not in already_scanned:
+                            to_scan.append(subgroup)
+                else:
+                    group_members = set(lr.get(f'/schoolclasses/{group}').get('sophomorixMembers', []))
+
+                members = members.union(group_members)
+
+            to_scan = to_scan[1:]
+
+        self.membersCount = len(members)
+        self.all_members = list(members)
