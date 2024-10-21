44fe4bc34f85c39c783a5f9c90306c5fd9c8a313
kiarn <arnaud@linuxmuster.net>
2024-10-21 21:33:20 +0200

    Init library

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/__init__.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/__init__.py
@@ -1,3 +1,14 @@
 from .user import *
+from .printer import *
+from .managementgroup import *
+from .project import *
+from .device import *
+from .schoolclass import *
 
+
+DeviceWriter = LMNDeviceWriter()
+MgmtGroupWriter = LMNMGMTGroupWriter()
+PrinterWriter = LMNPrinterWriter()
+ProjectWriter = LMNProjectWriter()
+SchoolclassWriter = LMNSchoolclassWriter()
 UserWriter  = LMNUserWriter()
