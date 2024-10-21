6592d3b3e8e62fa7db8b8b13d1316a0bbed15d86
kiarn <arnaud@linuxmuster.net>
2024-10-21 21:12:40 +0200

    Add UserWriter.

---
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/user.py
@@ -0,0 +1,42 @@
+import logging
+
+from ..ldap_writer import LdapWriter
+from ..urls.ldaprouter import router
+
+class LMNUserWriter:
+
+    def __init__(self):
+        self.lw = LdapWriter()
+        self.lr = router
+
+    def setattr(self, name, **kwargs):
+        """
+        Middleware to check if the object exists.
+
+        :param name: cn of the object
+        :type name: basestring
+        """
+
+        details = self.lr.get(f'/users/{name}')
+
+        if not details:
+            logging.info(f"The user {name} was not found in ldap.")
+            raise Exception(f"The user {name} was not found in ldap.")
+
+        self.lw._setattr(details, **kwargs)
+
+    def delattr(self, name, **kwargs):
+        """
+        Middleware to check if the object exists.
+
+        :param name: cn of the object
+        :type name: basestring
+        """
+
+        details = self.lr.get(f'/users/{name}')
+
+        if not details:
+            logging.info(f"The user {name} was not found in ldap.")
+            raise Exception(f"The user {name} was not found in ldap.")
+
+        self.lw._delattr(details, **kwargs)
