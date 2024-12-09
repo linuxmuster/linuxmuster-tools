f460b40eec2726aadf470ea8043a63eb6556085f
kiarn <arnaud@linuxmuster.net>
2024-12-09 16:16:19 +0100

    Setup config for default-school.

---
+++ usr/lib/python3/dist-packages/linuxmusterTools/lmnconfig/setup.py
@@ -0,0 +1,18 @@
+import os
+import logging
+
+from linuxmusterTools.lmnfile import LMNFile
+
+
+class SetupConfig:
+
+    def __init__(self, school='default-school'):
+        # TODO: setup path for multischool ?
+        setup_config_path = f'/var/lib/linuxmuster/setup.ini'
+
+        if os.path.isfile(setup_config_path) and school == 'default-school':
+            with LMNFile(setup_config_path, 'r') as config:
+                self.config = config.read()
+        else:
+            logging.warning(f"No setup config found for the school {school}")
+            self.config = {}
