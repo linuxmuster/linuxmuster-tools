1bdd490cdda629f2858c6d0c441ca6ba4e50cb42
kiarn <arnaud@linuxmuster.net>
2024-12-09 14:34:08 +0100

    Get full status of attic users.

--- usr/lib/python3/dist-packages/linuxmusterTools/common/attic.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/common/attic.py
@@ -1,6 +1,8 @@
 import os
 import logging
+import datetime
 
+from linuxmusterTools.lmnconfig import SophomorixConfig
 from linuxmusterTools.ldapconnector import LMNLdapReader as lr
 from .convert import convert_sophomorix_time
 
@@ -10,12 +12,12 @@ def get_killdate(user, school='default-school'):
     Get kill date of an user, if it exists.
     Actually only works with default-school on a single school instance.
 
-    :param user:
-    :type user:
-    :param school:
-    :type school:
-    :return:
-    :rtype:
+    :param user: user login
+    :type user: basestring
+    :param school: school of the user
+    :type school: basestring
+    :return: Date when the account was killed, if it was the case
+    :rtype: basestring
     """
 
 
@@ -32,7 +34,71 @@ def get_killdate(user, school='default-school'):
     except Exception as e:
         killlog.close()
         logging.warning(str(e))
+        return None
+    return None
 
+def get_attic_status(user, school='default-school'):
+    """
+    Get the actual status of an user in attic.
+    Actually only works with default-school on a single school instance.
+
+    :param user: user login
+    :type user: basestring
+    :param school: school of the user
+    :type school: basestring
+    :return: toleration, deactivation or killable status and date
+    :rtype:
+    """
+
+
+    if school != 'default-school':
+        logging.warning("This functionality only works on default-school.")
+
+    details = lr.get(f'/users/{user}')
+
+    result = {'status': 'No information found.', 'start':'', 'end':''}
+
+    if not details:
+        killdate = get_killdate(user, school=school)
+        if killdate is not None:
+            result['status'] = "killed"
+            result['start'] = killdate
+            result['end'] = killdate
+            return result
+        else:
+            return result
+
+    if details['sophomorixAdminClass'] != 'attic':
+        logging.warning(f"User {user} is not an attic user, exiting.")
+        result['status'] = "Activated"
+        return result
+
+    status = details['sophomorixStatus']
+    admin_file = details['sophomorixAdminFile']
+    sophomorix_config = SophomorixConfig().config
+    role_config = sophomorix_config.get(f'userfile.{admin_file}', {})
+
+    if status == "M" or status == "T":
+        # Status Managed or Tolerates
+        start = datetime.datetime.strptime(details['sophomorixTolerationDate'], '%Y%m%d%H%M%S.%fZ')
+        result['start'] = start.strftime("%d %b %Y %H:%M:%S")
+        result['status'] = "tolerated"
+        result['end'] = (start + datetime.timedelta(days=int(role_config['TOLERATION_TIME']))).strftime("%d %b %Y %H:%M:%S")
+    elif status == "D" or status == "L":
+        # Status Deactivated or Locked
+        start = datetime.datetime.strptime(details['sophomorixDeactivationDate'], '%Y%m%d%H%M%S.%fZ')
+        result['start'] = start.strftime("%d %b %Y %H:%M:%S")
+        result['status'] = "deactivated"
+        result['end'] = (start + datetime.timedelta(days=int(role_config['DEACTIVATION_TIME']))).strftime("%d %b %Y %H:%M:%S")
+    elif status == "R" or status == "K":
+        # Status Removable or Killable
+        # Same start and end time
+        start = datetime.datetime.strptime(details['sophomorixDeactivationDate'], '%Y%m%d%H%M%S.%fZ')
+        result['start'] = (start + datetime.timedelta(days=int(role_config['DEACTIVATION_TIME']))).strftime("%d %b %Y %H:%M:%S")
+        result['status'] = "killable"
+        result['end'] = (start + datetime.timedelta(days=int(role_config['DEACTIVATION_TIME']))).strftime("%d %b %Y %H:%M:%S")
+
+    return result
 
 def check_attic_dir(school='default-school'):
     """
@@ -53,8 +119,10 @@ def check_attic_dir(school='default-school'):
 
     result = {}
     for user in os.listdir(attic_dir):
-        if not lr.get(f'/users/{user}'):
-            killdate = get_killdate(user)
-            result[user] = killdate
+        result[user] = get_attic_status(user, school=school)
+        # if not lr.get(f'/users/{user}'):
+        #     killdate = get_killdate(user)
+        #     result[user] = killdate
 
     return result
+
