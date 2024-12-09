21b34b12db4b8216330f5ea3d12ae4e9fe7932d3
kiarn <arnaud@linuxmuster.net>
2024-12-09 12:34:48 +0100

    Load sophomorix config.

---
+++ usr/lib/python3/dist-packages/linuxmusterTools/lmnconfig/sophomorix.py
@@ -0,0 +1,20 @@
+import os
+import logging
+
+from linuxmusterTools.lmnfile import LMNFile
+
+
+class SophomorixConfig:
+
+    def __init__(self, school='default-school'):
+        prefix = ""
+        if school != 'default-school':
+            prefix = f"{school}."
+        sophomorix_config_path = f'/etc/linuxmuster/sophomorix/{school}/{prefix}school.conf'
+
+        if os.path.isfile(sophomorix_config_path):
+            with LMNFile(sophomorix_config_path, 'r') as config:
+                self.config = config.read()
+        else:
+            logging.warning(f"No sophomorix config found for the school {school}")
+            self.config = {}
