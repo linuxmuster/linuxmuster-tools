c1a5301e2fc3a7002fe2edbff0055168ce51c8d1
kiarn <arnaud@linuxmuster.net>
2024-12-15 15:49:35 +0100

    Add groupname check.

--- usr/lib/python3/dist-packages/linuxmusterTools/common/checks/names.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/common/checks/names.py
@@ -7,6 +7,7 @@ NAME_RULES = {
     'password': re.compile(r"^[a-zA-Z0-9?!@#§+\-$%&*{}()\]\[]+$"),
     'strong_password': re.compile(r"(?=.*[a-z])(?=.*[A-Z])(?=.*[?!@#§+\-$%&*{}()]|(?=.*\d)).{7,}"),
     'project': re.compile(r"^[a-z0-9_\-]*$"),
+    'group': re.compile(r"^[a-z0-9_\-]*$"),
     "session": re.compile(r"^[a-z0-9\+\-_]*$", re.IGNORECASE),
     "linbo_conf": re.compile(r"^[a-z0-9\+\-_]*$", re.IGNORECASE),
     "linbo_image": re.compile(r"^[a-zA-Z0-9_\-]+$"),
