1d14e5d42f476e3d4c6ebf21ee48d61a2f93fa51
kiarn <arnaud@linuxmuster.net>
2025-03-24 12:06:38 +0100

    Remove parent from a parent group.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/user.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/user.py
@@ -72,6 +72,7 @@ class LMNUserWriter:
 
     def add_parents(self, name, parents=[]):
         """
+        Add parents to the group STUDENT-parents
 
         :param name: cn of the student
         :type name: basestring
@@ -99,6 +100,37 @@ class LMNUserWriter:
         parentgroup_dn = student['dn'].replace(name, f"{name}-parents")
         for dn in parents_dn:
             self.ow.add_member(parentgroup_dn, dn)
+
+    def remove_parents(self, name, parents=[]):
+        """
+        Remove parents from the group STUDENT-parents
+
+        :param name: cn of the student
+        :type name: basestring
+        :param parents: List of cn of the parents
+        :type parents: list
+        """
+
+        # Check if the users exist
+        student = self.lr.get(f'/users/{name}')
+
+        if student.get('sophomorixRole', None) != 'student':
+            logging.info(f'{name} is not a student, can not add parent.')
+            return
+
+        parents_dn = []
+        for parent in parents:
+            details = self.lr.get(f'/users/{parent}')
+            # Allowing all roles but student
+            if details.get('sophomorixRole', 'student') == 'student':
+                logging.info(f'{parent} do not have a valid role to be parent.')
+                return
+            # At this point, we have a valid role for a parent
+            parents_dn.append(details['dn'])
+
+        parentgroup_dn = student['dn'].replace(name, f"{name}-parents")
+        for dn in parents_dn:
+            self.ow.remove_member(parentgroup_dn, dn)
 
 
 
