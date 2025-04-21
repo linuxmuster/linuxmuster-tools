5b992ec5e34a85f466d58a4e7458606ebd3d7ccb
kiarn <arnaud@linuxmuster.net>
2025-04-21 22:37:43 +0200

    Search for one specific GPO.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/urls/system.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/urls/system.py
@@ -6,11 +6,25 @@ from linuxmusterTools.ldapconnector.urls.ldaprouter import router, SCHOOL_MARKER
 def get_all_gpos():
     """
     Get all Group Policies Objects
-    Return a a list of LMNGPO data object.
+    Return a list of LMNGPO data object.
     """
 
     ldap_filter = f"""(&
                                 (objectClass=groupPolicyContainer)
+                            )"""
+
+    return ldap_filter
+
+@router.single(r'/gpos/(?P<name>[\w\-_: ]*)', models.LMNGPO)
+def get_one_gpo(name):
+    """
+    Get one specific Group Policy Object after his displayName
+    Return a LMNGPO data object.
+    """
+
+    ldap_filter = f"""(&
+                                (objectClass=groupPolicyContainer)
+                                (displayName={name})
                             )"""
 
     return ldap_filter
