7232b0c7acc2b5019c06e60728988f734a8e63af
kiarn <arnaud@linuxmuster.net>
2025-05-05 13:06:22 +0200

    Common group writer class.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/group.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/group.py
@@ -2,44 +2,113 @@ import logging
 
 from ..ldap_writer import LdapWriter
 from ..urls.ldaprouter import router
+from linuxmusterTools.common import lprint, spinner
+from linuxmusterTools.common.checks import NameChecker
+from ..models import LMNGroupModel
 
 
 logger = logging.getLogger(__name__)
+name_checker = NameChecker()
 
-class LMNGroup:
+class LMNGroupCommon:
+    """
+    Comme group class for all group types in the linuxmuster project (Projects,
+    Schoolclasses, ManagementGroup, Sophomorix Group, etc ...).
+    """
 
-    def __init__(self):
+    def __init__(self, cn):
+
+        # TODO if new is enabled
+        # if not name_checker.check_login_name(cn):
+        #     raise Exception(f"{cn} is not a valid CN")
+        # Missing methods _create and _move
+
+        self.cn = cn
         self.lw = LdapWriter()
         self.lr = router
+        self.model = LMNGroupModel
+        self.data = {}
+        self.load_data()
 
-    def setattr(self, name, **kwargs):
+    def load_data(self):
+        self.data = self.lr.get(f'/units/{self.cn}')
+
+        if not self.data:
+            raise Exception(f"The group {self.cn} was not found in ldap.")
+
+    def setattr(self, **kwargs):
         """
-        Middleware to check if the object exists.
-
-        :param name: cn of the object
-        :type name: basestring
-        """
-
-        details = self.lr.get(f'/groups/{name}')
-
-        if not details:
-            logger.info(f"The group {name} was not found in ldap.")
-            raise Exception(f"The group {name} was not found in ldap.")
-
-        self.lw._setattr(details, **kwargs)
-
-    def delattr(self, name, **kwargs):
-        """
-        Middleware to check if the object exists.
-
-        :param name: cn of the object
-        :type name: basestring
+        Set some attributes of the object directly in Ldap,
+        only for an existing object.
+        kwargs must contain a data dict with attributes/values to set.
         """
 
-        details = self.lr.get(f'/groups/{name}')
 
-        if not details:
-            logger.info(f"The group {name} was not found in ldap.")
-            raise Exception(f"The group {name} was not found in ldap.")
+        self.lw._setattr(self, **kwargs)
+        self.load_data()
 
-        self.lw._delattr(details, **kwargs)
+    def delattr(self, **kwargs):
+        """
+        Delete some attributes of the object directly in Ldap,
+        only for an existing object.
+        kwargs must contain a data dict with attributes/values to set.
+        """
+
+
+        self.lw._delattr(self, **kwargs)
+        self.load_data()
+
+    def getattr(self, attr):
+        """
+        Get a specific attribute of the object.
+        """
+
+
+        return self.data.get(attr, None)
+
+    def remove_member(self, user):
+        user_dn = self.lr.getval(f'/users/{user}', 'dn')
+
+        if not user_dn:
+            logger.info(f"The user {user} was not found in ldap.")
+            raise Exception(f"The object {user} was not found in ldap.")
+
+        try:
+            members = self.data['member']
+            if user_dn in members:
+                members.remove(user_dn)
+            else:
+                logging.info(f"{user} is not a member of schoolclass {self.cn}")
+                return
+            self.lw._setattr(self, data={'member': members})
+        except ValueError as e:
+            logger.warning(f"Could not remove member {user_dn} from {self.cn}: {str(e)}")
+
+    def add_member(self, user):
+        user_dn = self.lr.getval(f'/users/{user}', 'dn')
+
+        if not user_dn:
+            logger.info(f"The user {user} was not found in ldap.")
+            raise Exception(f"The user {user} was not found in ldap.")
+
+        try:
+            members = self.data['member']
+            members.append(user_dn)
+            self.lw._setattr(self, data={'member': members})
+        except Exception as e:
+            logger.warning(f"Could not append member {user_dn} to {self.cn}: {str(e)}")
+
+
+class LMNGroup(LMNGroupCommon):
+    """
+    Class to handle the Sophomorix Groups (saved under the OU Projects)
+    """
+
+    def __init__(self, cn):
+        super().__init__(cn)
+
+    def load_data(self):
+        self.data = self.lr.get(f'/groups/{self.cn}')
+
+        if not self.data:
+            raise Exception(f"The group {self.cn} was not found in ldap.")
