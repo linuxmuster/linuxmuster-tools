d4faed18299cbced8ab73940944756e0e324cfbe
kiarn <arnaud@linuxmuster.net>
2025-05-05 15:25:19 +0200

    Add subgroups as attributes.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/schoolclass.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/schoolclass.py
@@ -9,11 +9,25 @@ from .group import LMNGroupCommon
 logger = logging.getLogger(__name__)
 name_checker = NameChecker()
 
+class LMNSchoolclassGroup(LMNGroupCommon):
+
+    def __init__(self, cn):
+        super().__init__(cn)
+
+    def load_data(self):
+        self.data = self.lr.get(f'/units/{self.cn}')
+
+        if not self.data:
+            logging.warning(f"The group {self.cn} was not found in ldap.")
+
 class LMNSchoolclass(LMNGroupCommon):
 
     def __init__(self, cn):
         super().__init__(cn)
         self.model = LMNSchoolClassModel
+        self.students_group = LMNSchoolclassGroup(f"{self.cn}-students")
+        self.teachers_group = LMNSchoolclassGroup(f"{self.cn}-teachers")
+        self.parents_group = LMNSchoolclassGroup(f"{self.cn}-parents")
 
     def load_data(self):
         self.data = self.lr.get(f'/schoolclasses/{self.cn}')
