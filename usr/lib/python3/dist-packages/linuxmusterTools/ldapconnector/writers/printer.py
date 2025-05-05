5daa47cd558255ce6c68e3799f2577f48aea2a13
kiarn <arnaud@linuxmuster.net>
2025-05-05 13:06:53 +0200

    New writer class for printers.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/printer.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/printer.py
@@ -1,45 +1,20 @@
 import logging
 
-from ..ldap_writer import LdapWriter
-from ..urls.ldaprouter import router
+from linuxmusterTools.common import lprint, spinner
+from linuxmusterTools.common.checks import NameChecker
+from .group import LMNGroupCommon
 
 
 logger = logging.getLogger(__name__)
+name_checker = NameChecker()
 
-class LMNPrinter:
+class LMNPrinter(LMNGroupCommon):
 
-    def __init__(self):
-        self.lw = LdapWriter()
-        self.lr = router
+    def __init__(self, cn):
+        super().__init__(cn)
 
-    def setattr(self, name, **kwargs):
-        """
-        Middleware to check if the object exists.
+    def load_data(self):
+        self.data = self.lr.get(f'/printers/{self.cn}')
 
-        :param name: cn of the object
-        :type name: basestring
-        """
-
-        details = self.lr.get(f'/printers/{name}')
-
-        if not details:
-            logger.info(f"The printer {name} was not found in ldap.")
-            raise Exception(f"The printer {name} was not found in ldap.")
-
-        self.lw._setattr(details, **kwargs)
-
-    def delattr(self, name, **kwargs):
-        """
-        Middleware to check if the object exists.
-
-        :param name: cn of the object
-        :type name: basestring
-        """
-
-        details = self.lr.get(f'/printers/{name}')
-
-        if not details:
-            logger.info(f"The printer {name} was not found in ldap.")
-            raise Exception(f"The printer {name} was not found in ldap.")
-
-        self.lw._delattr(details, **kwargs)
+        if not self.data:
+            raise Exception(f"The printer {self.cn} was not found in ldap.")
