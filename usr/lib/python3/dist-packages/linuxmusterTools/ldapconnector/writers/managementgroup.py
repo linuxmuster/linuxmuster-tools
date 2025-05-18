ea2d1ee1d5038bd8a6fbed7a8a29fd41d615ac43
kiarn <arnaud@linuxmuster.net>
2025-05-18 12:30:39 +0200

    New writer for mgmt group.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/managementgroup.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/managementgroup.py
@@ -1,45 +1,20 @@
 import logging
 
-from ..ldap_writer import LdapWriter
-from ..urls.ldaprouter import router
+from .group import LMNGroupCommon
 
 
 logger = logging.getLogger(__name__)
 
-class LMNMgmtGroup:
+class LMNMgmtGroup(LMNGroupCommon):
+    """
+    Class to handle the management groups
+    """
 
-    def __init__(self):
-        self.lw = LdapWriter()
-        self.lr = router
+    def __init__(self, cn):
+        super().__init__(cn)
 
-    def setattr(self, name, **kwargs):
-        """
-        Middleware to check if the object exists.
+    def load_data(self):
+        self.data = self.lr.get(f'/managementgroups/{self.cn}')
 
-        :param name: cn of the object
-        :type name: basestring
-        """
-
-        details = self.lr.get(f'/managementgroups/{name}')
-
-        if not details:
-            logger.info(f"The managementgroup {name} was not found in ldap.")
-            raise Exception(f"The managementgroup {name} was not found in ldap.")
-
-        self.lw._setattr(details, **kwargs)
-
-    def delattr(self, name, **kwargs):
-        """
-        Middleware to check if the object exists.
-
-        :param name: cn of the object
-        :type name: basestring
-        """
-
-        details = self.lr.get(f'/managementgroups/{name}')
-
-        if not details:
-            logger.info(f"The managementgroup {name} was not found in ldap.")
-            raise Exception(f"The managementgroup {name} was not found in ldap.")
-
-        self.lw._delattr(details, **kwargs)
+        if not self.data:
+            raise Exception(f"The group {self.cn} was not found in ldap.")
