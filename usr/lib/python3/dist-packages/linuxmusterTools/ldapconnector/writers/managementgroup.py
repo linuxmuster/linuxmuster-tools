c9539c4ebc41a91c86de4d4e21c357d1cbad6188
kiarn <arnaud@linuxmuster.net>
2025-05-18 14:27:02 +0200

    Support for multischool.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/managementgroup.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/writers/managementgroup.py
@@ -10,11 +10,11 @@ class LMNMgmtGroup(LMNGroupCommon):
     Class to handle the management groups
     """
 
-    def __init__(self, cn):
-        super().__init__(cn)
+    def __init__(self, cn, school='default-school'):
+        super().__init__(cn, school=school)
 
     def load_data(self):
-        self.data = self.lr.get(f'/managementgroups/{self.cn}')
+        self.data = self.lr.get(f'/managementgroups/{self.cn}', school=self.school)
 
         if not self.data:
             raise Exception(f"The group {self.cn} was not found in ldap.")
