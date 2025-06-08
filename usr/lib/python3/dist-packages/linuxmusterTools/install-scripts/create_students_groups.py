0184108811bb7d8a9741f6d09c809c19b02b9e90
kiarn <arnaud@linuxmuster.net>
2025-06-08 12:32:40 +0200

    Consolidate tests for a new installation.

--- usr/lib/python3/dist-packages/linuxmusterTools/install-scripts/create_students_groups.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/install-scripts/create_students_groups.py
@@ -1,17 +1,24 @@
 #! /opt/linuxmuster/bin/python3
 
+import os
 from linuxmusterTools.ldapconnector import LMNLdapReader as lr, LMNParentsGroup, LMNSchoolclass
 from linuxmusterTools.common import lprint
 
 
-# Checking groups like 7a-teachers, 7a-parents, 7a-students
-for c in lr.getval('/schoolclasses','cn'):
-    lprint.info(f"Checking students groups from schoolclass {c}")
-    l = LMNSchoolclass(c)
-    l.fill_group_members()
+if not os.path.isfile('/etc/linuxmuster/webui/config.yml'):
+    lprint.info('New installation detected, I will not check the LDAP groups.')
+else:
+    try:
+        # Checking groups like 7a-teachers, 7a-parents, 7a-students
+        for c in lr.getval('/schoolclasses','cn'):
+            lprint.info(f"Checking students groups from schoolclass {c}")
+            l = LMNSchoolclass(c)
+            l.fill_group_members()
 
-# Checking OU Student-Parents through creating a dummy LMNParentsGroup
-p = LMNParentsGroup('')
+        # Checking OU Student-Parents through creating a dummy LMNParentsGroup
+        p = LMNParentsGroup('')
+    except Exception as e:
+        lprint.danger(str(e))
 
 
 
