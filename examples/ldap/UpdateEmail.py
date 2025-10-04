804a57e380109ad0043283c19fa3251aee2b2302
kiarn <arnaud@linuxmuster.net>
2025-10-04 22:36:53 +0200

    Update hook to update teacher's mail.

--- examples/ldap/UpdateEmail.py
+++ examples/ldap/UpdateEmail.py
@@ -1,23 +1,36 @@
 #! /usr/bin/env python3
 
 """
-Script to update all email addresses in the attribute proxyAddresses
+Script to update all teacher's email addresses in the attribute mail
 with the template lastname-firstname@mydomain.school.
+You can replace the email in the attribute proxyAddresse through the same way.
+In order to keep all teacher's email up-to-date, you can put this script in
+/etc/linuxmuster/sophomorix/default-school/hooks/sophomorix-update.d and in
+/etc/linuxmuster/sophomorix/default-school/hooks/sophomorix-add.d
+
+(this will be executed after each sophomorix user's change in Ldap).
 """
 
-from linuxmusterTools.ldapconnector import LMNLdapReader as lr, UserWriter as uw
+from linuxmusterTools.ldapconnector import LMNTeachers
 
-# Get a list of all teachers
-# (only the attributes cn, givenName and sn - 
-# to get a list of all teachers with all attributes, just use
-# lr.get('/roles/teachers')
+# Get a list of all teachers, but with write permissions in Ldap!
+# Be careful to not change attributes if you not know what you are doing.
 
-teachers = lr.get('/roles/teacher', attributes=['cn', 'givenName', 'sn'])
+teachers = LMNTeachers()
 
 domain = "mydomain.school"
 
 # Email template lastname-firstname@mydomain.school
-for teacher in teachers:
-    email = f"{teacher['sn']}-{teacher['givenName']}@{domain}"
-    uw.setattr(teacher['cn'], data={'proxyAddresses':email})
+for cn, teacher in teachers.items():
+
+    # Shortcut for all teacher's data
+    data = teacher.data
+    lastname = data['sn'].lower()
+    firstname = data['sn'].lower()
+
+    # Prepare a new email like lastname-firstname@mydomain.school
+    new_mail = f"{lastname}-{firstname}@{domain}"
+
+    # Save in LDAP
+    teacher.setattr(data={'mail': new_mail})
 
