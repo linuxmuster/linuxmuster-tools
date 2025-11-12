20135c6413130525ca5a54a03c11dd336a8d04de
kiarn <arnaud@linuxmuster.net>
2025-11-12 11:35:43 +0100

    Function to print students list of a schoolclass.

---
+++ usr/lib/python3/dist-packages/linuxmusterTools/print/__init__.py
@@ -0,0 +1,1 @@
+from .schoolclasses import *
---
+++ usr/lib/python3/dist-packages/linuxmusterTools/print/schoolclasses.py
@@ -0,0 +1,40 @@
+from ..ldapconnector import LMNLdapReader as lr
+from .render import LatexRenderer
+
+def print_schoolclass_list(schoolclass, caller, school='default_school', template="datalist-DE-32-template.tex"):
+    """
+    Print a list of students from a specific schoolclass.
+
+    :param caller: the user calling the process, for generating the filename
+    :return: PDF path
+    """
+
+
+    # use displayname if defined instead of school cn ?
+    # choice for templates !
+
+    data = []
+    students = lr.getval(f'/schoolclasses/{schoolclass}', 'sophomorixMembers')
+
+    if not students:
+        raise Exception(f"Schoolclass {schoolclass} not found or empty!")
+
+    for student in students:
+        details = lr.get(f'/users/{student}', dict=False)
+        data.append({'lastname':details.sn, 'firstname':details.givenName})
+
+    data_sorted = sorted(data, key=lambda item: item['lastname']+item['firstname'])
+
+    l = LatexRenderer(template, "schoolclass", data_sorted, caller, vars={"school":school, "schoolclass":schoolclass})
+    return l.compile()
+
+def print_schoolclasses_list(schoolclasses, caller, school='default_school', template="datalist-DE-32-template.tex"):
+    """
+    Print a list of students from multiple schoolclasses.
+
+    :param schoolclasses: list of schoolclasses
+    :param caller: the user calling the process, for generating the filename
+    :return: PDF path
+    """
+
+    pass
