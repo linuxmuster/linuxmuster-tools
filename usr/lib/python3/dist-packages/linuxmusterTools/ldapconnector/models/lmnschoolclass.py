598122cbaaa603ff6d64d821d26b7e72484cfe45
kiarn <arnaud@linuxmuster.net>
2025-11-16 10:56:16 +0100

    Allow custom fields in schoolclasses.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/lmnschoolclass.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/lmnschoolclass.py
@@ -21,6 +21,16 @@ class LMNSchoolClassModel(LMNModel):
     sophomorixAddQuota: list
     sophomorixAdmins: list
     sophomorixCreationDate: str # datetime
+    sophomorixCustom1: str
+    sophomorixCustom2: str
+    sophomorixCustom3: str
+    sophomorixCustom4: str
+    sophomorixCustom5: str
+    sophomorixCustomMulti1: list
+    sophomorixCustomMulti2: list
+    sophomorixCustomMulti3: list
+    sophomorixCustomMulti4: list
+    sophomorixCustomMulti5: list
     sophomorixHidden: bool
     sophomorixJoinable: bool
     sophomorixMailAlias: bool
