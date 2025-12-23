9da9cd4d5fd0bac7a738d3c99b6352284dc29ed3
kiarn <arnaud@linuxmuster.net>
2025-12-23 15:29:53 +0100

    Add basic smbclient for common operations. Experimental.

---
+++ usr/lib/python3/dist-packages/linuxmusterTools/smbclient/__init__.py
@@ -0,0 +1,1 @@
+from .smbclient import *
---
+++ usr/lib/python3/dist-packages/linuxmusterTools/smbclient/smbclient.py
@@ -0,0 +1,71 @@
+"""
+A smbclient utility to perform some basic operations on the fileserver.
+"""
+
+
+# Actually still in experimental mode
+from ..lmnconfig import SAMBA_DOMAIN
+import subprocess
+
+class LMNSMBClient:
+    def __init__(self, school='default-school'):
+        self.school = school
+
+    def _execute(self, subcmd):
+        with open('/etc/linuxmuster/.secret/administrator', 'r') as admpwd:
+            passwd = admpwd.readline().strip()
+
+        cmd = [
+            "/bin/smbclient",
+            f"-U administrator%{passwd}",
+            f"//{SAMBA_DOMAIN}/{self.school}",
+            "-c",
+            subcmd,
+        ]
+
+        result = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+        out, err = result.communicate()
+
+        # Clean up memory
+        passwd = ''
+        cmd = []
+        result = ''
+
+        return out
+
+    def list(self, path):
+        """
+        Return a list of files and directories in path, if existing.
+
+        :param path: Samba path like students/10a/myuser/transfer
+        :return: list
+        """
+
+
+        cmd = f"dir \"{path}/*\";"
+        lines = self._execute(cmd).decode().split('\n')
+
+        filelist = []
+        for idx, line in enumerate(lines):
+            details = line.split()
+            # [filename/dirname, type, size, split date...]
+            try:
+                name = details[0]
+
+                # Unfortunately smbclient doesn't always throw an error, and the return code may be 0
+                # even if there was a problem. So it's necessary to check manually if the first result is '.'
+                # If not, the first line is mostly the error.
+
+                if idx == 0 and name != '.':
+                    raise Exception(f'There was a problem with the list command: {line}')
+
+                objtype = 'file' if details[1] != 'D' else 'directory' # Should be 'N' for files
+                filelist.append({'name': name, 'type': objtype, 'size': details[2]})
+            except IndexError:
+                # Should be the end of list
+                break
+
+        return filelist
+
+
+
