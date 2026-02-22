fc6977e51e4ceb2333df82faef53bb94c64c58cd
kiarn <arnaud@linuxmuster.net>
2026-02-22 14:44:37 +0100

    Filter given parameters.

--- usr/lib/python3/dist-packages/linuxmusterTools/linbo/models.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/linbo/models.py
@@ -1,3 +1,5 @@
+import inspect
+import logging
 from dataclasses import dataclass
 
 
@@ -9,6 +11,20 @@ class Partition:
     Id: int
     Label: str
     Size: str
+
+    @classmethod
+    def from_dict(cls, kwargs):
+        endkwargs = {}
+        config = kwargs['config']
+        del kwargs['config']
+        for k, v in kwargs.items():
+            for p in inspect.signature(cls).parameters:
+                if k.lower() == p.lower():
+                    endkwargs[p] = v
+                    break
+            else:
+                logging.warning(f"Parameter {k} is not an Partition parameter in {config}.")
+        return cls(**endkwargs)
 
 @dataclass
 class OS:
@@ -27,22 +43,51 @@ class OS:
     StartEnabled: bool = True
     SyncEnabled: bool = True
 
+    @classmethod
+    def from_dict(cls, kwargs):
+        endkwargs = {}
+        config = kwargs['config']
+        del kwargs['config']
+        for k, v in kwargs.items():
+            for p in inspect.signature(cls).parameters:
+                if k.lower() == p.lower():
+                    endkwargs[p] = v
+                    break
+            else:
+                logging.warning(f"Parameter {k} is not an OS parameter in {config}.")
+        return cls(**endkwargs)
+
 @dataclass
 class Linbo:
     Cache: str
     Group: str
-    KernelOptions: str
     AutoFormat: bool = False
     AutoInitCache: bool = False
     AutoPartition: bool = False
     GuiDisabled: bool = False
+    KernelOptions: str = ''
     Locale: str = 'de-de'
     RootTimeout: int = 600
     DownloadType: str = 'torrent'
     UseMinimalLayout: bool = False
 
+    @classmethod
+    def from_dict(cls, kwargs):
+        endkwargs = {}
+        config = kwargs['config']
+        del kwargs['config']
+        for k, v in kwargs.items():
+            for p in inspect.signature(cls).parameters:
+                if k.lower() == p.lower():
+                    endkwargs[p] = v
+                    break
+            else:
+                logging.warning(f"Parameter {k} is not an Linbo parameter in {config}.")
+        return cls(**endkwargs)
+
 @dataclass
 class LinboConfig:
     LINBO: Linbo
     Partitions: list
+    path: str
     OS: list
