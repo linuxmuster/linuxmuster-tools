3d7d6546fc73b00396a1dcbc7bb6072575485231
kiarn <arnaud@linuxmuster.net>
2026-04-23 10:14:49 +0200

    Move futures classes to WIP.

--- usr/lib/python3/dist-packages/linuxmusterTools/linbo/__init__.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/linbo/__init__.py
@@ -7,17 +7,23 @@ from .command import *
 from .wol import *
 from .host_status import *
 from .boot_logs import *
-from .hooks import *
-from .multicast import *
-from .torrent import *
 from .ssh import *
-from .terminal import *
-from .firmware import *
-from .drivers import *
-from .kernel import *
 from .linbofs import *
-from .wlan import *
-from .image_sync import *
-from .linbo_update import *
 from .grub_generator import *
 from .dhcp import *
+
+## WIP
+
+#from .hooks import *
+#from .multicast import *
+#from .torrent import *
+#from .terminal import *
+#from .firmware import *
+#from .drivers import *
+#from .kernel import *
+#from .wlan import *
+#from .image_sync import *
+#from .linbo_update import *
+
+
+
