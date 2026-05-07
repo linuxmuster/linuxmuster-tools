e64c1e4fd0673821ab33629628bf2824bab753c4
kiarn <arnaud@linuxmuster.net>
2026-05-07 13:10:13 +0200

    Refactor user models.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/lmnrawuser.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/models/lmnrawuser.py
@@ -1,13 +1,12 @@
 from dataclasses import dataclass, field, InitVar
-import re
-from .lmnsession import LMNSessionModel
 from .common import LMNModel
+from .lmnusermixin import LMNUserMixin
 
 from linuxmusterTools.common import WEBUI_IMPORT
 
 
 @dataclass
-class LMNRawUserModel(LMNModel):
+class LMNRawUserModel(LMNUserMixin, LMNModel):
     cn: str
     custom_fields_config: InitVar[dict]
     displayName: str
@@ -90,109 +89,6 @@ class LMNRawUserModel(LMNModel):
     school:         str  = field(init=False)
     webfilter:      bool = field(init=False)
     wifi:           bool = field(init=False)
-
-    @staticmethod
-    def _check_schoolclass_number(s):
-        n = re.findall(r'\d+', s)
-        if n:
-            return int(n[0])
-        else:
-            return 10000000 # just a big number to come after all schoolclasses
-
-    def extract_schoolclasses(self, membership):
-        schoolclasses = []
-        for dn in membership:
-            if 'OU=Students' in dn \
-                and '-teachers,' not in dn \
-                and '-parents,' not in dn \
-                and '-students,' not in dn:
-                schoolclass = self.common_name(dn)
-                if schoolclass:
-                    schoolclasses.append(schoolclass)
-        schoolclasses = sorted(schoolclasses, key=lambda s: (self._check_schoolclass_number(s), s))
-        return schoolclasses
-
-    def extract_projects(self, membership):
-        projects = []
-        for dn in membership:
-            if 'OU=Projects' in dn:
-                project = self.common_name(dn)
-                if project:
-                    projects.append(project)
-        projects.sort()
-        return projects
-
-    def extract_printers(self, membership):
-        printers = []
-        for dn in membership:
-            if 'OU=printer-groups' in dn:
-                printer = self.common_name(dn)
-                if printer:
-                    printers.append(printer)
-        printers.sort()
-        return printers
-
-    def extract_management(self):
-        school_prefix = ""
-        if self.sophomorixSchoolname != 'default-school':
-            school_prefix = f"{self.sophomorixSchoolname}-"
-
-        for group in ['internet', 'intranet', 'printing', 'webfilter', 'wifi']:
-            setattr(self, group, False)
-            for dn in self.memberOf:
-                if dn.startswith(f"CN={school_prefix}{group},OU=Management"):
-                    setattr(self, group, True)
-
-    def parse_sessions(self):
-        self.lmnsessions = []
-        for v in self.sophomorixSessions:
-            data = v.split(';')
-            members = data[2].split(',') if data[2] else []
-            membersCount = len(members)
-            self.lmnsessions.append(LMNSessionModel(data[0], data[1], members, membersCount))
-
-    def parse_exam(self):
-        if not self.sophomorixExamMode:
-            self.examMode = False
-            self.examTeacher = ''
-            self.examBaseCn = ''
-        elif self.sophomorixExamMode[0] == '---':
-            self.examMode = False
-            self.examTeacher = ''
-            self.examBaseCn = ''
-        else:
-            self.examMode = True
-            self.examTeacher = self.sophomorixExamMode[0]
-            self.examBaseCn = self.cn.replace('-exam', '')
-
-    def create_custom_fields_objects(self, custom_config={}):
-        self.customFields = {}
-
-        proxy_add = custom_config.get('proxyAddresses', {}).get(self.sophomorixRole, {'editable': False, 'show': False, 'title':''})
-        self.customFields['proxyAddresses'] = {
-            'title': proxy_add['title'],
-            'canRead': proxy_add['show'],
-            'canWrite': proxy_add['editable'],
-            'value': self.proxyAddresses
-        }
-
-        for i in range(1, 5):
-            config = custom_config.get('custom', {}).get(self.sophomorixRole, {}).get(str(i), {'editable': False, 'show': False, 'title':''})
-            self.customFields[f"sophomorixCustom{i}"] = {
-                'title': config['title'],
-                'canRead': config['show'],
-                'canWrite': config['editable'],
-                'value': getattr(self, f"sophomorixCustom{i}")
-            }
-
-        for i in range(1, 5):
-            config = custom_config.get('customMulti', {}).get(self.sophomorixRole, {}).get(str(i), {'editable': False, 'show': False, 'title': ''})
-            self.customFields[f"sophomorixCustomMulti{i}"] = {
-                'title': config['title'],
-                'canRead': config['show'],
-                'canWrite': config['editable'],
-                'value': getattr(self, f"sophomorixCustomMulti{i}")
-            }
 
     def __post_init__(self, custom_fields_config={}):
         self.schoolclasses = self.extract_schoolclasses(self.memberOf)
