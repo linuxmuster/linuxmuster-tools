765f66432b39b09a8c3c0b9e935f0d47fe6d9e22
kiarn <arnaud@linuxmuster.net>
2026-05-08 10:03:12 +0200

    Ensure same timezone for comparisons.

--- usr/lib/python3/dist-packages/linuxmusterTools/common/checks/certificates.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/common/checks/certificates.py
@@ -34,7 +34,7 @@ class DomCert:
     def _get_limit(self):
         self.notAfter = self.cert['notAfter']
         self.notAfter_timestamp = datetime.strptime(self.notAfter,"%b %d %H:%M:%S %Y GMT")
-        self.valid = (self.notAfter_timestamp - datetime.now()).days > 0
+        self.valid = (self.notAfter_timestamp - datetime.utcnow()).days > 0
 
     def isvalid(self):
         self.load()
@@ -43,6 +43,6 @@ class DomCert:
     def expires(self):
         self.load()
         if self.valid:
-            return f"{(self.notAfter_timestamp - datetime.now()).days} days ({self.notAfter})"
+            return f"{(self.notAfter_timestamp - datetime.utcnow()).days} days ({self.notAfter})"
 
         return f"Expired since {self.notAfter}"
