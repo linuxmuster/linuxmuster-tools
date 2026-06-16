e00e2f795667db8ce680465820a21b0fd230ef68
kiarn <arnaud@linuxmuster.net>
2026-06-16 17:12:55 +0200

    Update changelog.

--- usr/lib/python3/dist-packages/linuxmusterTools/__init__.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/__init__.py
@@ -1,11 +1,9 @@
+import sys
 import logging
+from pathlib import Path
 
-logger = logging.getLogger(__name__)
-logger_handler = logging.StreamHandler()
-logger.addHandler(logger_handler)
-logger.setLevel(logging.INFO)
 
-class LMNToolsFormatter(logging.Formatter):
+class LMNToolsLoggingFormatter(logging.Formatter):
     grey = '\x1b[38;5;245m'
     green = '\x1b[38;5;82m'
     orange = '\x1b[38;5;214m'
@@ -31,4 +29,62 @@ class LMNToolsFormatter(logging.Formatter):
         formatter = logging.Formatter(log_fmt, "%Y-%m-%d %H:%M:%S")
         return formatter.format(record)
 
-logger_handler.setFormatter(LMNToolsFormatter())
+
+class LMNFileHandler(logging.Handler):
+    """
+    Routes log records to per-submodule files under /var/log/linuxmuster/lmntools/.
+    """
+
+    LOG_DIR = Path('/var/log/linuxmuster/lmntools')
+    file_formatter = logging.Formatter(
+        '%(asctime)s %(levelname)8s %(name)s %(filename)s:%(lineno)d - %(message)s',
+        '%Y-%m-%d %H:%M:%S'
+    )
+
+    def __init__(self):
+        super().__init__()
+        self._handlers = {}
+        self.LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+    def _get_handler(self, submodule):
+        if submodule not in self._handlers:
+            fh = logging.FileHandler(self.LOG_DIR / f'{submodule}.log')
+            fh.setFormatter(self.file_formatter)
+            self._handlers[submodule] = fh
+        return self._handlers[submodule]
+
+    def emit(self, record):
+        parts = record.name.split('.')
+        submodule = parts[1] if len(parts) > 1 else 'lmntools'
+        self._get_handler(submodule).emit(record)
+
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+logger.propagate = False
+
+
+def setup_logging(mode='file'):
+    """
+    Configure logging for linuxmusterTools.
+
+    mode='file'    : log to /var/log/linuxmuster/lmntools/<submodule>.log
+    mode='console' : log to stderr with colored output
+    """
+
+
+    logger.handlers.clear()
+    logger.propagate = False
+    logger.setLevel(logging.INFO)
+
+    if mode == 'console':
+        handler = logging.StreamHandler()
+        handler.setFormatter(LMNToolsLoggingFormatter())
+    else:
+        handler = LMNFileHandler()
+
+    logger.addHandler(handler)
+
+
+if hasattr(sys, 'ps1') or sys.flags.interactive:
+    setup_logging(mode='console')
