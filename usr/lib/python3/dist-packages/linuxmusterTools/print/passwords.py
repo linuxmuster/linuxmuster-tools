55536dd19cf45e7db8c264207d5a69eb09a860ec
kiarn <arnaud@linuxmuster.net>
2026-07-14 20:38:37 +0200

    Print passwords and convert sophomorix templates as jinja templates.

---
+++ usr/lib/python3/dist-packages/linuxmusterTools/print/passwords.py
@@ -0,0 +1,65 @@
+from .render import LatexRenderer
+from .templates import *
+from ..lmnconfig import SchoolConfig, SAMBA_WORKGROUP
+
+
+def print_passwords_list(schoolclass, caller, school='default-school', template="passwords-DE-1-template.tex"):
+    """
+    Print the password letters/cards of the students from a specific schoolclass.
+
+    :param caller: the user calling the process, for generating the filename
+    :return: PDF path
+    """
+
+    templates = LatexTemplates().templates
+
+    if template not in templates:
+        raise Exception(f"Can not find the template {template}!")
+
+    template_obj = templates[template]
+
+    students = lr.getval(f'/schoolclasses/{schoolclass}', 'sophomorixMembers', school=school)
+
+    if not students:
+        raise Exception(f"Schoolclass {schoolclass} not found or empty!")
+
+    data = []
+    for student in students:
+        details = lr.get(f'/users/{student}', as_dict=False, school=school)
+        data.append({
+            'lastname': details.sn,
+            'firstname': details.givenName,
+            'login': details.sAMAccountName,
+            'password': details.sophomorixFirstPassword,
+            'schoolclass': schoolclass,
+        })
+
+    data_sorted = sorted(data, key=lambda item: item['lastname']+item['firstname'])
+
+    teachers = lr.getval(f'/schoolclasses/{schoolclass}', 'sophomorixAdmins', school=school) or []
+    teachermembers = ", ".join(
+        f"{teacher.givenName} {teacher.sn}"
+        for teacher in (lr.get(f'/users/{login}', as_dict=False, school=school) for login in teachers)
+    )
+
+    sophomorix_config = SchoolConfig(school=school).config.get('school', {})
+
+    vars = {
+        "schoolclass": schoolclass,
+        "school_longname": sophomorix_config.get('SCHOOL_LONGNAME', ''),
+        "filename": f"{caller}-passwords-{schoolclass}",
+        "admins_print": sophomorix_config.get('ADMINS_PRINT', ''),
+        "domain": SAMBA_WORKGROUP,
+        "teachermembers": teachermembers,
+        "urlstart_print": sophomorix_config.get('URLSTART_PRINT', ''),
+        "urlstart_comment_print": sophomorix_config.get('URLSTART_COMMENT_PRINT', ''),
+        "urlschuko_print": sophomorix_config.get('URLSCHUKO_PRINT', ''),
+        "urlschuko_comment_print": sophomorix_config.get('URLSCHUKO_COMMENT_PRINT', ''),
+        "urlmail_print": sophomorix_config.get('URLMAIL_PRINT', ''),
+        "urlmail_comment_print": sophomorix_config.get('URLMAIL_COMMENT_PRINT', ''),
+        "urlmoodle_print": sophomorix_config.get('URLMOODLE_PRINT', ''),
+        "urlmoodle_comment_print": sophomorix_config.get('URLMOODLE_COMMENT_PRINT', ''),
+    }
+
+    l = LatexRenderer(template_obj, data_sorted, caller, vars=vars)
+    return l.compile()
--- usr/lib/python3/dist-packages/linuxmusterTools/print/render.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/print/render.py
@@ -72,6 +72,8 @@ class LatexRenderer:
 
         if self.template_obj.type == "schoolclass":
             output_file = f"{self.caller}-schoolclass-{self.vars['schoolclass']}.tex"
+        elif self.template_obj.type == "passwords":
+            output_file = f"{self.caller}-passwords-{self.vars['schoolclass']}.tex"
 
         self.destination_file = os.path.join(OUTPUT_DIR, output_file)
 
