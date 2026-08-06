89084010af82c08c87e34bd7dd4ddc0a29ad0cc3
kiarn <arnaud@linuxmuster.net>
2026-08-06 19:32:24 +0200

    Fix images object.

--- usr/lib/python3/dist-packages/linuxmusterTools/linbo/images.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/linbo/images.py
@@ -540,7 +540,7 @@ class LinboImageManager:
             if diff:
                 # Only delete a differential image
                 self.groups[group].diff_image.delete()
-            elif date in self.images[group].backups:
+            elif date in self.groups[group].backups:
                 # The object to delete is only a backup
                 self.groups[group].backups[date].delete()
                 self.groups[group].load()
