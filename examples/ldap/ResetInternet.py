1f3105a2148f022aa3dd7420ae793bec53544b53
kiarn <arnaud@linuxmuster.net>
2026-08-07 16:18:51 +0200

    Use GroupManager instead of LMNMgmtGroup.

--- examples/ldap/ResetInternet.py
+++ examples/ldap/ResetInternet.py
@@ -7,16 +7,16 @@ permissions set in sessions of teachers.
 """
 
 from linuxmusterTools.common import lprint
-from linuxmusterTools.ldapconnector import LMNLdapReader as lr, LMNMgmtGroup
+from linuxmusterTools.ldapconnector import LMNLdapReader as lr
+from linuxmusterTools.samba_util import GroupManager
 
 
 # Get a "raw" list of all students ("raw" means without parents attributes, to speed up the process).
 
 students = lr.get('/rawroles/student')
-total = len(students)
-internet_group = LMNMgmtGroup('internet')
+to_fix = [student['cn'] for student in students if not student['internet']]
 
-for idx, student in enumerate(students):
-    if not student['internet']:
-        lprint.success(f"Fixing internet for student {student['cn']}")
-        internet_group.add_member(student['cn'])
+if to_fix:
+    for cn in to_fix:
+        lprint.success(f"Fixing internet for student {cn}")
+    GroupManager().add_members('internet', to_fix)
